0c796d9a7dbdd53f46d1866485ad3b9558e43076
Mateusz Ramotowski <mateusz.ramotowski@profil-software.com>
2023-06-17 10:41:29 +0200

    ROW-13 Simple footer responsive 300px - 1920px

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,13 @@
 import styles from "./Footer.module.scss";
 
 export const Footer = () => {
-  return <footer className={styles.footer}>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      Copyright © 2023 by{" "}
+      <a href="https://mateusz-ramotowski-poland.github.io/portfolio-webpage/" target="_blank">
+        Mateusz Ramotowski
+      </a>
+      . All rights reserved
+    </footer>
+  );
 };
