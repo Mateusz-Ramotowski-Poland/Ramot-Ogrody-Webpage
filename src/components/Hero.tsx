035a79daae008889f9091ed643fb5715c9693ec0
Mateusz Ramotowski <mateusz.ramotowski@profil-software.com>
2023-07-14 21:34:26 +0200

    ROW-14 Hero responsive 300px - 1920px - carousel logic

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,66 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import hero1 from "../images/hero1.jpg";
 import hero2 from "../images/hero2.jpg";
 import hero3 from "../images/hero3.jpg";
 import { CarouselIndicators } from "../components";
 import styles from "./Hero.module.scss";
 
+const carouselItems = [
+  {
+    description: "Dokładność w pracach ogrodniczych",
+    imgAlt: "kwitnące drzewo w ogrodzie",
+    imgSrc: hero1,
+  },
+  {
+    description: "Płatność za ułożone drzewo",
+    imgAlt: "ułożone drzewo do palenia",
+    imgSrc: hero2,
+  },
+  {
+    description: "Profesjonalny sprzęt i bezpieczeństwo",
+    imgAlt: "pilarz ścinający drzewo",
+    imgSrc: hero3,
+  },
+];
+
 export const Hero = () => {
   const [state, setState] = useState(0);
+  const [width, setWidth] = useState(window.innerWidth);
+  const maxState = 2;
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (width < 768) {
+      const id = setInterval(() => {
+        if (state < maxState) {
+          setState(state + 1);
+        } else {
+          setState(0);
+        }
+      }, 3000);
+
+      return () => {
+        clearInterval(id);
+      };
+    }
+  });
+
   return (
     <div className={styles.hero}>
       <div className={styles.container_smallest_sm}>
         <h1 className={styles.title}>Ramot ogrody</h1>
-        <p className={styles.paragraph_container}>Profesjonalny sprzęt i bezpieczeństwo</p>
-        <img src={hero1} alt="kwitnące drzewo w ogrodzie" className={styles.photo}></img>
+        <p className={styles.paragraph_container}>{carouselItems[state].description}</p>
+        <img src={carouselItems[state].imgSrc} alt={carouselItems[state].imgAlt} className={styles.photo}></img>
       </div>
 
       <div className={styles.container_md_xxxlg}>
@@ -28,7 +76,7 @@ export const Hero = () => {
           <img src={hero3} alt="pilarz ścianający drzewo" className={styles.photo}></img>
         </ul>
       </div>
-      <CarouselIndicators maxState={2} state={state} setState={setState} />
+      <CarouselIndicators maxState={maxState} state={state} setState={setState} />
     </div>
   );
 };
