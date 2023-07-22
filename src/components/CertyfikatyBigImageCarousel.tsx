d238e2cf3611d81405f8af35833ade99c2da0e59
Mateusz Ramotowski <mateusz.ramotowski@profil-software.com>
2023-07-22 20:55:04 +0200

    ROW-31 Exit carousel after mouse clicking and escape key

--- src/components/CertyfikatyBigImageCarousel.tsx
+++ src/components/CertyfikatyBigImageCarousel.tsx
@@ -3,6 +3,7 @@ import { Image } from "../pages/Certyfikaty";
 import { IonIcon } from "@ionic/react";
 import { closeOutline } from "../../node_modules/ionicons/icons";
 import { CarouselArrows, CarouselIndicators } from "../components";
+import { useEffect, useRef } from "react";
 
 interface Props {
   carouselImage: number;
@@ -12,9 +13,31 @@ interface Props {
 }
 
 export const CertyfikatyBigImageCarousel = (props: Props) => {
+  const containerRef = useRef<HTMLElement>(null);
+  const exitRef = useRef<HTMLIonIconElement>(null);
+
+  const handleContainerClick = (event: React.MouseEvent) => {
+    const clicked = event.target as HTMLElement;
+    if (clicked === containerRef.current || clicked === exitRef.current) {
+      props.setIsCarouselVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    const exitCarousel = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        props.setIsCarouselVisible(false);
+      }
+    };
+
+    window.addEventListener("keydown", exitCarousel);
+
+    return () => window.removeEventListener("keydown", exitCarousel);
+  }, []);
+
   return (
-    <section className={styles.container}>
-      <IonIcon icon={closeOutline} className={styles.icon} />
+    <section className={styles.container} onClick={handleContainerClick} ref={containerRef}>
+      <IonIcon icon={closeOutline} className={styles.icon} ref={exitRef} />
       <CarouselArrows maxState={props.images.length - 1} setState={props.setCarouselImage} state={props.carouselImage} />
       <img
         key={props.images[props.carouselImage].id}
