8a9228b27f1d3e491fa0ef92e563cab243a1b88b
Mateusz Ramotowski <mateusz.ramotowski@profil-software.com>
2023-07-26 16:20:45 +0200

    ROW-31 Added Plus minus icons logic

--- src/components/CertyfikatyBigImageCarousel.tsx
+++ src/components/CertyfikatyBigImageCarousel.tsx
@@ -14,12 +14,37 @@ interface Props {
 export const CertyfikatyBigImageCarousel = (props: Props) => {
   const containerRef = useRef<HTMLElement>(null);
   const exitRef = useRef<HTMLIonIconElement>(null);
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  const imgScale = [1, 1.2, 1.44, 1.7, 2];
+  const imgScaleIndex = useRef<number>(0);
+  const imgTransformTranslate = useRef<number[]>([0, 0]);
 
   const handleContainerClick = (event: React.MouseEvent) => {
     const clicked = event.target as HTMLElement;
     if (clicked === containerRef.current || clicked === exitRef.current) {
       props.setIsCarouselVisible(false);
     }
+  };
+
+  const changeImageStyle = () => {
+    (imgRef.current as HTMLImageElement).style.transform = `scale(${imgScale[imgScaleIndex.current]}) translate(${
+      imgTransformTranslate.current[0]
+    }px, ${imgTransformTranslate.current[1]}px)`;
+  };
+
+  const handlePlusClick = () => {
+    if (imgScaleIndex.current < imgScale.length - 1) {
+      imgScaleIndex.current += 1;
+    }
+    changeImageStyle();
+  };
+
+  const handleMinusClick = () => {
+    if (imgScaleIndex.current > 0) {
+      imgScaleIndex.current -= 1;
+    }
+    changeImageStyle();
   };
 
   useEffect(() => {
@@ -36,8 +61,8 @@ export const CertyfikatyBigImageCarousel = (props: Props) => {
 
   return (
     <section className={styles.container} onClick={handleContainerClick} ref={containerRef}>
-      <IonIcon icon={addCircleOutline} className={styles.icon_plus} />
-      <IonIcon icon={removeCircleOutline} className={styles.icon_minus} />
+      <IonIcon icon={addCircleOutline} className={styles.icon_plus} onClick={handlePlusClick} />
+      <IonIcon icon={removeCircleOutline} className={styles.icon_minus} onClick={handleMinusClick} />
       <IonIcon icon={closeOutline} className={styles.icon_exit} ref={exitRef} />
 
       <CarouselArrows maxState={props.images.length - 1} setState={props.setCarouselImage} state={props.carouselImage} />
@@ -46,6 +71,7 @@ export const CertyfikatyBigImageCarousel = (props: Props) => {
         src={props.images[props.carouselImage].source}
         alt={props.images[props.carouselImage].alt}
         className={styles.img}
+        ref={imgRef}
       />
       <CarouselIndicators maxState={props.images.length - 1} setState={props.setCarouselImage} state={props.carouselImage} styles="certyfikaty" />
     </section>
